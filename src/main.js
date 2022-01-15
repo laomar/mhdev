d7a7e0672f29999ed617657d894945da90fd856e
rustchan <gorust@qq.com>
2022-01-15 21:22:01 +0800

    创建项目

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import { createApp } from 'vue';
+import ElementPlus from 'element-plus';
+import axios from 'axios';
+import cookie from 'vue3-cookies';
+import qs from 'qs';
+import router from './router';
+import App from './App.vue';
+import './index.css';
+import 'element-plus/dist/index.css';
+
+const app = createApp(App);
+
+app.use(ElementPlus);
+app.use(router);
+app.use(cookie);
+app.config.globalProperties.$http = axios;
+app.config.globalProperties.$qs = qs;
+app.mount('#app');
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  content: ['./src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: 'media',
+  theme: {
+    extend: {}
+  },
+  plugins: []
+};
